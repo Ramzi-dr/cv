353e0f257a2f8ffd2e3333e1a801d1da43b37b68
Ramzi-dr <82414280+Ramzi-dr@users.noreply.github.com>
2021-11-01 01:14:46 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,40 @@
+
+function getid(porto){
+  console.log(porto.id);
+  var porto = '#'+porto.id;
+  console.log(porto)
+
+
+
+$(porto).on('click', function(){
+  // if already full screen; exit
+  // else go fullscreen
+  console.log(porto);
+  if (
+    document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullscreenElement
+  ) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  } else {
+    element = $(porto).get(0);
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+  }
+})};
